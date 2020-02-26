cb3b30186e33700ff16922ae741896d065cfc530
Cory Loughran <loughrancory@gmail.com>
2020-02-25 20:00:55 -0500

    Added current balance to account and simple updating function when adding transactions

--- budgeteer_app/admin.py
+++ budgeteer_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 #from django.contrib.auth.models import User
-from .models import Account, AccountType
+from .models import Account, AccountType, Transaction, TransactionType, TransactionCategory
 
 # Register your models here.
 #admin.site.register(User)
 admin.site.register(Account)
 admin.site.register(AccountType)
+admin.site.register(Transaction)
+admin.site.register(TransactionType)
+admin.site.register(TransactionCategory)
--- budgeteer_app/models.py
+++ budgeteer_app/models.py
@@ -8,11 +8,57 @@ class AccountType(models.Model):
     def __str__(self):
         return self.account_type
 
+
 class Account(models.Model):
     account_name = models.CharField(max_length=100)
     account_type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     starting_balance = models.DecimalField(max_digits=12, decimal_places=2)
+    current_balance = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __str__(self):
         return self.account_name
+
+    def transaction(self, amount):
+        self.current_balance += amount
+        self.save()
+
+
+class TransactionType(models.Model):
+    type_name = models.CharField(name="type", max_length=100)
+
+    def __str__(self):
+        return self.type
+
+
+class TransactionCategory(models.Model):
+    transaction_type = models.ForeignKey(TransactionType, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    transaction_type = models.ForeignKey(TransactionType, on_delete=models.CASCADE)
+    category = models.ForeignKey(TransactionCategory, on_delete=models.CASCADE)
+    notes = models.CharField(max_length=250)
+
+    def __str__(self):
+        return str(self.date) + " | " + str(self.amount) + ": " + self.notes
+
+    def save(self, *args, **kwargs):
+        amount = self.amount
+        print(self.transaction_type == "Expense", amount, type(str(self.transaction_type)))
+
+        if str(self.transaction_type) == "Expense" or (self.transaction_type == "Transfer" and self.category[:2] == "To"):
+            amount = -amount
+            print("Here")
+
+        print(amount)
+        self.account.transaction(amount)
+
+        super().save(*args, **kwargs)
