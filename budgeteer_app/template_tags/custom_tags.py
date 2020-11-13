d9375f8dc507c1d8113689a9d966b603d418252a
Cory Loughran <loughrancory@gmail.com>
2020-11-12 20:51:59 -0400

    Style changes on budget view

--- budgeteer_app/template_tags/custom_tags.py
+++ budgeteer_app/template_tags/custom_tags.py
@@ -3,3 +3,8 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def sub(num1, num2):
+    return num1 - num2
